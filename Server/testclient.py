6a0f717cf355d300d5f6816bc499920b3083cfc1
alex rzehak <alexander.rzehak@gmx.de>
2018-12-07 22:37:52 +0100

    Make testclient.py PEP8 compliant

--- Server/testclient.py
+++ Server/testclient.py
@@ -3,9 +3,10 @@ import requests
 
 
 def main():
-    payload = dict(tags=['weiblicher_darsteller', 'beume'], content=['PLACEHOLDER'])
+    payload = dict(tags=['weiblicher_darsteller', 'beume'],
+                   content=['PLACEHOLDER'])
     r = requests.post('http://localhost:5000/add_cardbox', json=payload)
-    
+
 
 if __name__ == "__main__":
     main()
